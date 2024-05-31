5e171cd8c553adaec9f0d9ddb905e202de160b9d
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-05-31 20:11:45 +0700

    add ui-tests

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ sut = Solution()
 def get_form():
     return '''
             <html>
+            <head>
+                <title>This is demo app</title>
+            </head>
             <body>
             <h1>Check if courses can be finished</h1>
             <form action="/" method="POST">
                 Количество курсов: <input type="number" name="numCourses"><br>
                 Требования (простое разделение, e.g. 1,0;2,1(; ставится если есть еще зависимости): <input type="text" name="prerequisites"><br>
-                <input type="submit" value="Submit">
+                <input id="go" type="submit" value="Submit">
             </form>
             </body>
             </html>
@@ -27,8 +30,11 @@ def can_finish():
     try:
         numCourses = int(request.form['numCourses'])
         prerequisites_str = request.form['prerequisites']
-        prerequisites = [list(map(int, pair.split(','))) for pair in prerequisites_str.split(';')]
-        return {'result': sut.canFinish(numCourses, prerequisites)}
+        if prerequisites_str == '':
+            return {'result': sut.canFinish(numCourses, [])}
+        else:
+            prerequisites = [list(map(int, pair.split(','))) for pair in prerequisites_str.split(';')]
+            return {'result': sut.canFinish(numCourses, prerequisites)}
     except TypeError:
         abort(400)
     except ValueError:
